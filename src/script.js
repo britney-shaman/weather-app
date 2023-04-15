52036386d91955da86bcc66445d9328967b10f7a
britney-shaman <130091959+britney-shaman@users.noreply.github.com>
2023-04-15 14:44:31 -0700

    Import weekly forecast API information and use it to display weekly temperatures

--- src/script.js
+++ src/script.js
@@ -40,27 +40,45 @@ city.addEventListener("submit", changeCity);
 //Create a function that formats the info from the weather API and displays it to the user
 function showTemperature(location) {
   console.log(location);
+  document.querySelector("#day-1").innerHTML = Math.round(
+    location.data.daily[1].temperature.day
+  );
+  document.querySelector("#day-2").innerHTML = Math.round(
+    location.data.daily[2].temperature.day
+  );
+  document.querySelector("#day-3").innerHTML = Math.round(
+    location.data.daily[3].temperature.day
+  );
+  document.querySelector("#day-4").innerHTML = Math.round(
+    location.data.daily[4].temperature.day
+  );
+  document.querySelector("#day-5").innerHTML = Math.round(
+    location.data.daily[5].temperature.day
+  );
+  document.querySelector("#day-6").innerHTML = Math.round(
+    location.data.daily[6].temperature.day
+  );
   document.querySelector("#heading").innerHTML = location.data.city;
   document.querySelector("#temp-display").innerHTML = Math.round(
-    location.data.temperature.current
+    location.data.daily[0].temperature.day
   );
   let mainIcon = document.querySelector("#main-icon");
-  mainIcon.src = location.data.condition.icon_url;
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    location.data.temperature.feels_like
-  );
+  mainIcon.src = location.data.daily[0].condition.icon_url;
+  //document.querySelector("#feels-like").innerHTML = Math.round(
+  //location.data.temperature.feels_like
+  // );
   document.querySelector("#humidity").innerHTML = Math.round(
-    location.data.temperature.humidity
+    location.data.daily[0].temperature.humidity
   );
   document.querySelector("#wind-speed").innerHTML = Math.round(
-    location.data.wind.speed
+    location.data.daily[0].wind.speed
   );
   document.querySelector("#description").innerHTML =
-    location.data.condition.description;
+    location.data.daily[0].condition.description;
   document.querySelector("#current-date").innerHTML = new Date(
-    location.data.time * 1000
+    location.data.daily[0].time * 1000
   );
-  celciusTemperature = Math.round(location.data.temperature.current);
+  celciusTemperature = Math.round(location.data.daily[0].temperature.day);
 }
 
 //Organize the users location information and send it to the API, then call to the function showTemperature that displays the API weather information
@@ -92,3 +110,17 @@ function youAreHere(event) {
 //Create a variable for the green 'current' button and call the function showTemperature when it is clicked
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", youAreHere);
+
+//Create a function that calls to the weekly forcast API when a city is searched
+function changeCity(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#user-input").value;
+  let apiKey = "dob4d22a920ef88t300f64e56eab54e2";
+  let inputUrl =
+    "https://api.shecodes.io/weather/v1/forecast?query=" +
+    userInput +
+    "&key=" +
+    apiKey +
+    "&units=metric";
+  axios.get(inputUrl).then(showTemperature);
+}
